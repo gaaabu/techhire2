78d19e9a2d9d0ffa318e749f0ffef7b030377840
gaaabu <elepanogabriel2004@gmail.com>
2025-05-26 18:07:57 +0800

    third commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <link href="{{ asset('css/about-styles.css') }}" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
@@ -28,7 +29,7 @@
                         <a class="nav-link" href="/contact">Contact Us</a>
                     </li>
                 </ul>
-                
+
                 <ul class="navbar-nav">
                     @auth
                         <li class="nav-item dropdown">
@@ -57,12 +58,17 @@
                             <a class="nav-link" href="{{ route('login') }}">Login</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">Register</a>
+                            <a class="btn btn-register ms-2" href="{{ route('register') }}">Register</a>
                         </li>
                     @endauth
                 </ul>
             </div>
         </div>
     </nav>
+    <section class="hero text-center">
+        <div class="container">
+            <h1 class="display-4 fw-bold mb-3">About Us</h1>
+        </div>
+    </section>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/about', function (){
 });
 
 Route::get('/contact', function (){
-    return view('about');
+    return view('contact');
 });
 // Public job show route - MOVED TO THE END
 Route::get('/jobs/{job}', [JobPostController::class, 'show'])->name('jobs.show');
